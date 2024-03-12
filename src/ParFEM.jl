24d4103280d681372421f9ab84beb6529a0c9a3b
Petr Krysl <pkrysl@ucsd.edu>
2024-03-12 13:37:40 -0700

    make tests visible

--- src/ParFEM.jl
+++ src/ParFEM.jl
@@ -15,4 +15,9 @@ include("parallel_assembly.jl")
 include("domain_decomposition.jl")
 include("high_level.jl")
 
+# Enable LSP look up in test modules
+if false
+    include("../test/runtests.jl")
+end
+
 end # module ParFEM
