8bb1c49d7809318d4c96720936c2dd97af98ca90
Petr Krysl <pkrysl@ucsd.edu>
2024-03-12 11:05:17 -0700

    add generic high level

--- src/high_level.jl
+++ src/high_level.jl
@@ -1,4 +1,9 @@
 
+"""
+    parallel_make_csc_matrix(fes, u, crsubdom, matrixcomputation!, ntasks)
+
+Assemble a CSC matrix.
+"""
 function parallel_make_csc_matrix(fes, u, crsubdom, matrixcomputation!, ntasks)
     femms = subdomainfemms(fes, ntasks, crsubdom)
     assembler = make_assembler(femms, SysmatAssemblerSparse, u)
@@ -6,5 +11,26 @@ function parallel_make_csc_matrix(fes, u, crsubdom, matrixcomputation!, ntasks)
     assemblers = make_task_assemblers(femms, assembler, SysmatAssemblerSparse, u)
     parallel_matrix_assembly(femms, assemblers, matrixcomputation!)
     K = csc_matrix_pattern(fes, u)
+    return add_to_matrix!(K, assembler)
+end
+
+
+"""
+    parallel_make_matrix(fes, u, crsubdom, matrixcomputation!, ntasks, kind = :CSC)
+
+Assemble a sparse matrix.
+"""
+function parallel_make_matrix(fes, u, crsubdom, matrixcomputation!, ntasks, kind = :CSC)
+    @assert kind in [:CSC, :CSR]
+    femms = subdomainfemms(fes, ntasks, crsubdom)
+    assembler = make_assembler(femms, SysmatAssemblerSparse, u)
+    start_assembler!(assembler)
+    assemblers = make_task_assemblers(femms, assembler, SysmatAssemblerSparse, u)
+    parallel_matrix_assembly(femms, assemblers, matrixcomputation!)
+    if kind == :CSC
+        K = csc_matrix_pattern(fes, u)
+    elseif kind == :CSR
+        K = csr_matrix_pattern(fes, u)
+    end
     return add_to_matrix!(K, assembler)
 end
