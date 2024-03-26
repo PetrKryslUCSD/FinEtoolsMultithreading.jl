3d1bd1dbb02776d931aff3555ee6caf04fd74bf0
Petr Krysl <pkrysl@ucsd.edu>
2024-03-25 19:17:03 -0700

    improve naming

--- src/element_coloring.jl
+++ src/element_coloring.jl
@@ -10,7 +10,7 @@ function element_coloring(fes, e2e::E2EM, ellist::Vector{IT} = Int[]) where {E2E
     return element_coloring!(element_colors, unique_colors, color_counts, e2e.map, ellistit)
 end
 
-function __find_minimal_count(color_used, color_counts)
+function __find_of_min_count(color_used, color_counts)
     c = 0
     mincount = typemax(eltype(color_counts))
     @inbounds for k in eachindex(color_used)
@@ -22,7 +22,7 @@ function __find_minimal_count(color_used, color_counts)
     return c
 end
 
-function __find_first_zero(color_used)
+function __find_first_avail(color_used)
     @inbounds for k in eachindex(color_used)
         if color_used[k] == 0 
             return k
@@ -32,32 +32,30 @@ function __find_first_zero(color_used)
 end
 
 function element_coloring!(element_colors, unique_colors, color_counts, e2emap, ellist_iterator) 
-    __color_used = fill(zero(eltype(element_colors)), length(unique_colors))
+    color_used = fill(zero(eltype(element_colors)), length(unique_colors))
     for e in ellist_iterator
         if element_colors[e] == 0
-            __color_used .= 0
+            color_used .= 0
             for oe in e2emap[e]
                 c = element_colors[oe]
                 if c > 0
-                    __color_used[c] += 1
+                    color_used[c] += 1
                 end
             end
-            if sum(__color_used) == 0
-                c = argmin(color_counts)
+            if sum(color_used) == 0
+                c = __find_of_min_count(color_used, color_counts)
                 element_colors[e] = unique_colors[c]
                 color_counts[c] += 1
             else
-                first_not_used = findfirst(x -> x == 0, __color_used)
-                if first_not_used === nothing
+                if __find_first_avail(color_used) == 0
                     added = maximum(unique_colors) + 1
                     push!(unique_colors, added)
                     push!(color_counts, 0)
-                    push!(__color_used, 0)
-                    c = argmin(color_counts)
-                    element_colors[e] = unique_colors[c]
+                    push!(color_used, 0)
+                    element_colors[e] = added
                     color_counts[c] += 1
                 else
-                    c = __find_minimal_count(__color_used, color_counts)
+                    c = __find_of_min_count(color_used, color_counts)
                     element_colors[e] = unique_colors[c]
                     color_counts[c] += 1
                 end
