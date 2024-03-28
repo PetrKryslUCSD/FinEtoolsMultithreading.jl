23c63c71fee1c33da1926817495d1c0044457b96
Petr Krysl <pkrysl@ucsd.edu>
2024-03-28 10:57:19 -0700

    improved nomenclature

--- src/domain_decomposition.jl
+++ src/domain_decomposition.jl
@@ -1,5 +1,5 @@
 """
-    domain_decomposition(fes, coloring, createsubdomain, ntasks = Threads.nthreads())
+    domain_decomposition(fes, coloring, createsubd, ntasks = Threads.nthreads())
 
 Create finite element machines for the subdomains.
 
@@ -8,42 +8,43 @@ Create finite element machines for the subdomains.
 - `ntasks` = number of tasks (subdomains),
 - `element_colors` = array of element colors, one for each element,
 - `unique_colors` = array of the unique element colours,
-- `createsubdomain` = function to create one finite element machine 
+- `createsubd` = function to create one finite element machine 
     
     Example: 
     ```
-    function createsubdomain(fessubset)
+    function createsubd(fessubset)
         FEMMDeforLinear(MR, IntegDomain(fessubset, GaussRule(3, 2)), material)
     end
     ```
 
 Create a vector of vectors: for each unique color in the element coloring, the
 vector of the finite element machines is stored. The finite element machines are
-created using the function `createsubdomain`.
+created using the function `createsubd`.
 
 The matrix is created by going sequentially through the unique colors
 and then in parallel execute all the finite element machines for that color.
 """
-function domain_decomposition(fes, coloring, createsubdomain, ntasks = Threads.nthreads())
+function domain_decomposition(fes, coloring, createsubd, ntasks=Threads.nthreads())
     element_colors, unique_colors = coloring
     decomposition = []
     resize!(decomposition, length(unique_colors))
     Threads.@threads for i in eachindex(unique_colors)
         decomposition[i] =
-            _make_femms(fes, ntasks, createsubdomain, element_colors, unique_colors[i])
+            _make_femms(fes, ntasks, createsubd,
+                findall(c -> c == unique_colors[i], element_colors)
+            )
     end
     return decomposition
 end
 
-function _make_femms(fes, ntasks, createsubdomain, element_colors, color)
-    color_list = findall(x -> x == color, element_colors)
+function _make_femms(fes, ntasks, createsubd, color_list)
     subsetfes = subset(fes, color_list)
     chks = chunks(1:count(subsetfes), ntasks)
     sds = []
     resize!(sds, length(chks))
     for k = 1:length(chks)
         (ch, j) = chks[k]
-        sds[k] = createsubdomain(subset(subsetfes, ch))
+        sds[k] = createsubd(subset(subsetfes, ch))
     end
     return typeof(sds[1])[sds[k] for k in eachindex(sds)]
 end
