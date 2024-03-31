270e0a19e19419dfe12654f5204602a6623065ef
Petr Krysl <pkrysl@ucsd.edu>
2024-03-31 08:13:01 -0700

    add first implementation of assembler with lookup

---
+++ src/assembler_patt_w_lookup.jl
@@ -0,0 +1,135 @@
+using FinEtools
+using SparseArrays
+import FinEtools.AssemblyModule.eltype
+import FinEtools.AssemblyModule.startassembly!
+import FinEtools.AssemblyModule.assemble!
+import FinEtools.AssemblyModule.makematrix!
+
+"""
+    SysmatAssemblerSparsePattwLookup{IT, MBT, IBT} <: AbstractSysmatAssembler
+
+Type for assembling a sparse global matrix from elementwise matrices using a
+sparsity pattern (sparse matrix with all potential non-zeroes represented, but
+all those numbers are actually zero).
+
+!!! note
+
+    All fields of the datatype are private. The type is manipulated by the
+    functions `startassembly!`, `assemble!`, and `makematrix!`.
+"""
+mutable struct SysmatAssemblerSparsePattwLookup{TPATT<:SparseMatrixCSC} <: AbstractSysmatAssembler
+    _pattern::TPATT
+    _rows::Vector{Dict{Int, Int}}
+    _nomatrixresult::Bool
+    _force_init::Bool
+end
+
+function eltype(self::TPATT) where {TPATT<:SysmatAssemblerSparsePattwLookup}
+    eltype(self._pattern.nzval)
+end
+
+function SysmatAssemblerSparsePattwLookup(pattern::TPATT) where {TPATT<:SparseMatrixCSC}
+    rows = Dict{Int, Int}[]
+    resize!(rows, size(pattern, 2))
+    for c in axes(pattern, 2)
+        rows[c] = Dict{Int, Int}()
+        for k in pattern.colptr[c]:(pattern.colptr[c+1] - 1)
+            r = pattern.rowval[k]
+            rows[c][r] = k
+        end
+    end
+    return SysmatAssemblerSparsePattwLookup(pattern, rows, false, false)
+end
+
+"""
+    startassembly!(self::SysmatAssemblerSparsePattwLookup{T},
+        elem_mat_nrows::IT,
+        elem_mat_ncols::IT,
+        n_elem_mats::IT,
+        row_nalldofs::IT,
+        col_nalldofs::IT;
+        force_init = false
+        ) where {T, IT<:Integer}
+
+Start the assembly of a global matrix.
+
+The method makes buffers for matrix assembly. It must be called before
+the first call to the method `assemble!`.
+
+# Arguments
+- `elem_mat_nrows` = row dimension of the element matrix;
+- `elem_mat_ncols` = column dimension of the element matrix;
+- `n_elem_mats` = number of element matrices;
+- `row_nalldofs`= The total number of rows as a tuple;
+- `col_nalldofs`= The total number of columns as a tuple.
+
+This is a noop. The pattern has already been built, per our assumption.
+
+# Returns
+- `self`: the modified assembler.
+
+"""
+function startassembly!(
+    self::TPATT,
+    elem_mat_nrows::IT,
+    elem_mat_ncols::IT,
+    n_elem_mats::IT,
+    row_nalldofs::IT,
+    col_nalldofs::IT;
+    force_init = false,
+) where {TPATT<:SysmatAssemblerSparsePattwLookup,IT<:Integer}
+    if force_init
+        fill!(zero(eltype(self._pattern.nzval)), self._pattern.nzval)
+    end
+    return self
+end
+
+"""
+    assemble!(
+        self::SysmatAssemblerSparsePattwLookup,
+        mat::MT,
+        dofnums_row::IT,
+        dofnums_col::IT,
+    ) where {MT, IT}
+
+Assemble a rectangular matrix.
+"""
+function assemble!(
+    self::TPATT,
+    mat::MBT,
+    dofnums_row::CIT,
+    dofnums_col::CIT,
+) where {TPATT<:SysmatAssemblerSparsePattwLookup,MBT,CIT}
+    # Assembly of a rectangular matrix.
+    # The method assembles a rectangular matrix using the two vectors of
+    # equation numbers for the rows and columns.
+    nrows = length(dofnums_row)
+    ncolumns = length(dofnums_col)
+    size(mat) == (nrows, ncolumns) || error("Wrong size of matrix")
+    row_nalldofs, col_nalldofs = size(self._pattern)
+    for j = 1:ncolumns
+        dj = dofnums_col[j]
+        dj < 1 && error("Column degree of freedom < 1")
+        dj > col_nalldofs && error("Column degree of freedom > size")
+        for i = 1:nrows
+            di = dofnums_row[i]
+            di < 1 && error("Row degree of freedom < 1")
+            di > row_nalldofs && error("Row degree of freedom > size")
+            k = self._rows[dj][di]
+            self._pattern.nzval[k] += mat[i, j]
+        end
+    end
+    return self
+end
+
+"""
+    makematrix!(self::SysmatAssemblerSparsePattwLookup)
+
+Make a sparse matrix.
+
+The sparse matrix (i.e. a sparsity pattern with the nonzero values filled in) is returned.
+ 
+"""
+function makematrix!(self::TPATT) where {TPATT<:SysmatAssemblerSparsePattwLookup}
+    return self._pattern
+end
--- test/test_parallel.jl
+++ test/test_parallel.jl
@@ -1,3 +1,46 @@
+module mparallelassembly_assembler_w_lup_1
+using FinEtools
+using FinEtoolsMultithreading.Exports
+using FinEtoolsMultithreading: SysmatAssemblerSparsePattwLookup, startassembly!, assemble!, makematrix!
+using LinearAlgebra
+using Test
+
+function test()
+    W = 1.1
+    L = 12.0
+    t = 0.32
+    nl, nt, nw = 12, 33, 24
+    nl, nt, nw = 2, 3, 2
+    ntasks = 2
+
+    fens, fes = H8block(L, W, t, nl, nw, nt)
+    geom = NodalField(fens.xyz)
+    psi = NodalField(fill(1.0, count(fens), 1))
+    nl = collect(1:3)
+    setebc!(psi, nl, true, ones(Int, length(nl)), 0.0)
+    numberdofs!(psi)
+    v_f = gathersysvec(psi)
+
+    femm = FEMMBase(IntegDomain(fes, GaussRule(3, 2)))
+    n2e = FENodeToFEMap(fes.conn, count(fens))
+    n2n = FENodeToNeighborsMap(n2e, fes.conn)
+    K = sparse_symmetric_csc_pattern(psi.dofnums, nalldofs(psi), n2n, zero(eltype(psi.values)))
+    assmblr = SysmatAssemblerSparsePattwLookup(K)
+    K = bilform_diffusion(femm, assmblr, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    
+    K_ff = matrix_blocked_ff(K, nfreedofs(psi))
+    result = abs(v_f' * K_ff * v_f)
+
+    ass = SysmatAssemblerFFBlock(nfreedofs(psi))
+    K_ff2 = bilform_diffusion(FEMMBase(IntegDomain(fes, GaussRule(3, 2))), ass, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    @test norm(K_ff - K_ff2) / norm(K_ff2) <= 1.0e-5
+    @test abs(v_f' * K_ff2 * v_f - (result)) / (result) <= 1.0e-5
+    true
+end
+test()
+nothing
+end
+
 module mmmmaps1
 using FinEtools
 using FinEtoolsMultithreading.Exports
